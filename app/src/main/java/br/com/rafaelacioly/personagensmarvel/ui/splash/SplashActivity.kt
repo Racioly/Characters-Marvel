8db19b6d11737856dfe084f8ed8fd66c2fdc24ce
Rafael Acioly <rafael@app2all.com.br>
2020-04-16 20:11:27 -0300

    Screen Hide Navigation.

--- app/src/main/java/br/com/rafaelacioly/personagensmarvel/ui/splash/SplashActivity.kt
+++ app/src/main/java/br/com/rafaelacioly/personagensmarvel/ui/splash/SplashActivity.kt
@@ -3,6 +3,7 @@ package br.com.rafaelacioly.personagensmarvel.ui.splash
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.view.View
 import br.com.rafaelacioly.personagensmarvel.ui.mian.MainActivity
 import br.com.rafaelacioly.personagensmarvel.BaseActivity
 
@@ -13,7 +14,15 @@ class SplashActivity : BaseActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        configScreen()
         executionPause()
+    }
+
+    private fun configScreen(){
+        //Ocultando a barra de navegação
+        window.decorView.apply {
+            systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
+        }
     }
 
     private fun executionPause(){
